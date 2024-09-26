2ba752b8642d8307b921fad8434428b32bc3f694
Habiba kaleem <153904678+habibasmit@users.noreply.github.com>
2024-09-26 16:15:45 +0500

    Add files via upload

---
+++ chapter 1 to 59/Chapter one Alert/App.js
@@ -0,0 +1,29 @@
+alert("Error! please enter your valid password.");
+
+alert("Wellcome to JS Land....\nHappy codding");
+
+alert("Wellcome to JS Land.");
+
+alert("Happy codding.");
+
+alert("Hello... I can run JS through my web browser's console");
+
+
+  // var name24 = ("variables <br>")
+  // document.write(name24)
+  // var a = 5;
+  // var b = 4;
+  // var c = (15 + 5);
+  // document.write(a);
+  // document.write(a + b)
+  // document.write(c);
+  
+  // var name1, name2, age ;
+  //   name1 = "habiba";
+  //   name2 = "Rao";
+  //   age = "<br>19";
+  //   document.write("<hr>my name is \n");
+  //   document.write(name1);
+  //   document.write("<br>cast \n ");
+  //   document.write(name2);
+  //   document.write(age);
